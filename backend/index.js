5e47793cc7066ce7bbf568fd1d783ad12fd2aa2a
ayush patel <152726075+risingstar185@users.noreply.github.com>
2026-08-15 12:24:19 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://movie-recommendation-website-1-475f.onrender.com/",
     credentials: true,
   })
 );
